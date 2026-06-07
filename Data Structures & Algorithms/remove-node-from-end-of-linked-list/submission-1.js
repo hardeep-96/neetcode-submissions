1ff9e32c411a40096fb3d10f809d26b4fddd8da9
hardeep-96 <hardeep.cms@gmail.com>
2026-06-07 15:29:14 +0530

    Add: remove-node-from-end-of-linked-list - submission-1

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @param {number} n
+     * @return {ListNode}
+     */
+    removeNthFromEnd(head, n) {
+        let len = 0;
+        let curr = head;
+        while (curr) {
+            len++;
+            curr = curr.next
+        }
+
+        const removeIdx = len - n;
+
+        if (removeIdx === 0) {
+            let tmp = head;
+            head = head.next;
+            tmp.next = null;
+        } else {
+            let curr = head;
+            let count = 1;
+            while (count < removeIdx) {
+                curr = curr.next;
+                count++;
+            }
+
+            let prev = curr;
+            let delNode = curr.next;
+            prev.next = delNode.next;
+            delNode.next = null;
+        }
+
+        return head;
+    }
+}
